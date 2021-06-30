4c7f716d17a8f350a7324e3440dc54e7af5d5a65
Mubassir Hayat <mubassirhayat@gmail.com>
2021-06-30 13:23:28 +0500

    bug: fixing the check applicant status page

--- resources/views/check-applicant-status.blade.php
+++ resources/views/check-applicant-status.blade.php
@@ -17,7 +17,6 @@
                                     <th scope="col">CNIC / CRC</th>
                                     <th scope="col">Name</th>
                                     <th scope="col">Current Status</th>
-                                    <th scope="col"></th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -25,33 +24,11 @@
                                     <tr>
                                         <td scope="row" class="align-middle">{{ $applicant->cnic }}</td>
                                         <td class="align-middle">{{ $applicant->name }}</td>
-                                        <td class="align-middle">
-                                            @php
-                                                $assessment = $applicant->assessments()->where('user_id', \Auth::id())->count()
-                                            @endphp
-                                            @if(\Auth::user()->hasRole('Assessment') && $applicant->status == 2 && $assessment == 1)
-                                                Assessment In progress
-                                            @elseif(\Auth::user()->hasRole('Assessment') && $applicant->status == 9 && $assessment == 2)
-                                                Reassessment in progress
-                                            @else
-                                                {{ $applicant->applicationStatus->title }}
-                                            @endif
-                                        </td>
-                                        <td class="float-right align-middle">
-                                            @if(\Auth::user()->hasRole('Help Desk'))
-                                                <a href="{{ route('admin.applications.create', ['applicant_id' => $applicant->id]) }}" type="button" class="btn btn-outline-success">Upload Documents</a>
-                                            @endif
-                                            @if(\Auth::user()->hasRole('Assessment') && (($applicant->status == 2 && $assessment == 0) || $applicant->status == 9 && $assessment == 1) )
-                                                <a href="{{ route("admin.applications.assessment", [$applicant->id]) }}" type="button" class="btn btn-outline-success">Start Assessment</a>
-                                            @endif
-                                            @if(\Auth::user()->hasRole('CRPD') && $applicant->status == 3 )
-                                                <a href="{{ route("admin.applications.verification", [$applicant->id]) }}" type="button" class="btn btn-outline-success">Start Verification</a>
-                                            @endif
-                                        </td>
+                                        <td class="align-middle">{{ $applicant->applicationStatus->title }}</td>
                                     </tr>
                                 @empty
                                     <tr>
-                                        <td class="align-middle text-center" colspan="4"><span class="lead text-muted">Applicant not registered.</span></td>
+                                        <td class="align-middle text-center" colspan="3"><span class="lead text-muted">Applicant not registered.</span></td>
                                     </tr>
                                 @endforelse
                             </tbody>
