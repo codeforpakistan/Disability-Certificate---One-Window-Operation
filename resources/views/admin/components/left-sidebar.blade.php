081a14df901f12050662b2967aff8e62bc3c90ae
Mubassir Hayat <mubassirhayat@gmail.com>
2021-07-14 18:36:20 +0500

    bug: changed the icon for activity log

--- resources/views/admin/components/left-sidebar.blade.php
+++ resources/views/admin/components/left-sidebar.blade.php
@@ -98,11 +98,9 @@
                 <li class="nav-item {{ request()->routeIs('admin.activity-logs.*') ? "active" : "" }}">
                     <a class="nav-link" href="{{ route('admin.activity-logs.index') }}" >
                         <span class="nav-link-icon d-md-none d-lg-inline-block">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-check" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-activity" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <circle cx="9" cy="7" r="4"></circle>
-                                <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
-                                <path d="M16 11l2 2l4 -4"></path>
+                                <path d="M3 12h4l3 8l4 -16l3 8h4"></path>
                             </svg>
                         </span>
                         <span class="nav-link-title">Activity Logs</span>
